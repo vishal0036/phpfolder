84c66b7c71d70648df1242d7011316e63eb83b01
vishal0036 <vishal@fanaticcoders.com>
2024-03-27 12:45:11 +0530

    new file added

---
+++ php.php
@@ -0,0 +1,32 @@
+<?php
+
+//array mutliple value store in a single variable
+
+
+/// index array
+// jisme number me koi value atti hai 
+
+// assosative array
+// jisme key or value atti hai 
+
+
+$color = array("note" => 50, "book" =>60, "notebook" =>70);
+foreach($color as $key => $value){
+echo '<pre>';
+    echo "$key $value";
+}
+
+
+
+?>
+
+
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+
+<?php include 'practices.php';?>
+
+
+
+
