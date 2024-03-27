7112b4fc50ac9d7ab910d872dafe1f7da3213973
vishal0036 <vishal@fanaticcoders.com>
2024-03-27 12:48:22 +0530

    second file modify

--- practices.php
+++ practices.php
@@ -1,19 +1,6 @@
 <?php
 
-?>
-
-<form action="" method="post">
 
 
-<label for="practices.php">Name</label>
-<input type="text" name="name">
-<input type="email" name="email">
-<input type="passward" password="passward">
-<input type="submit">
-</form>
 
 
-<?php
-echo $_POST["name"];
-echo $_POST["email"];
-echo $_POST["passward"];
